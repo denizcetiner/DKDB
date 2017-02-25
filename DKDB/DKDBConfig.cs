ee1fb263404c9c454da14562db8ccc2fb1a6bd7e
Kerem <Kerem@DESKTOP-G9ODNBV>
2017-02-25 22:07:00 +0300

    Add project files.

---
+++ DKDB/DKDBConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKDB
+{
+    public abstract class DKDBConfig : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ DKDB/DbSet.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKDB
+{
+    public abstract class DbSet<T>
+    {
+        private List<T> recordsToAdd = new List<T>();
+        private List<T> recordsToRemove = new List<T>();
+        private List<T> recordsToUpdate = new List<T>();
+        private List<T> allRecords = new List<T>();
+
+        #region CRUD
+
+        public void Add(T record)
+        {
+            recordsToAdd.Add(record);
+        }
+
+        public void AddRange(IEnumerable<T> records)
+        {
+
+        }
+
+        public void Remove(T record)
+        {
+            recordsToRemove.Add(record);
+        }
+
+        public void RemoveRange(IEnumerable<T> records)
+        {
+
+        }
+
+        public void Update(T record)
+        {
+            recordsToUpdate.Add(record);
+        }
+
+        public void UpdateRange(IEnumerable<T> records)
+        {
+
+        }
+
+        private void ReadAllRecords()
+        {
+            if (allRecords.Count == 0)
+            {
+                
+            }
+        }
+
+        #endregion
+
+        #region Read
+
+        public long Count()
+        {
+            ReadAllRecords();
+            return allRecords.LongCount();
+        }
+
+        public T Last()
+        {
+            ReadAllRecords();
+            return allRecords.Last();
+        }
+
+        public T First(Func<T, bool> predicate)
+        {
+            ReadAllRecords();
+            return allRecords.First(predicate);
+        }
+
+        public IEnumerable<T> Where(Func<T, bool> predicate)
+        {
+            ReadAllRecords();
+            return allRecords.Where(predicate);
+        }
+
+        public List<T> All()
+        {
+            ReadAllRecords();
+            return allRecords;
+        }
+
+        #endregion
+
+        public int SaveChanges()
+        {
+
+        }
+    }
+}
