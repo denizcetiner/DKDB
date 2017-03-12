13613a4bb68cbc4ae78c2448defe93d0526c7ca0
denizcetiner <deniz.cetiner94@gmail.com>
2017-03-12 17:48:18 +0300

    Created FileOps, DKDBCustomAttribute classes, designed the update flow and added the base codes for some of these. Created a custom attribute and validator for it. Created some of the code for writing or updating a record in the specific database file. Design of recursive child object adding for database created and partially implemented. (Update might be improved in future.)

---
+++ DKDB/DKDBCustomAttributes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKDB
+{
+    public class DKDBCustomAttributes
+    {
+        public static List<PropertyInfo> GetReferencePropertyList(Type T, List<Type> ReferenceChecklist)
+        {
+            List<PropertyInfo> ReferencePropertyList = new List<PropertyInfo>();
+
+            PropertyInfo[] infos = T.GetProperties();
+            foreach (PropertyInfo info in infos)
+            {
+                if (ReferenceChecklist.Contains(info.GetType()))
+                {
+                    ReferencePropertyList.Add(info);
+                }
+            }
+
+            return ReferencePropertyList;
+        }
+
+        public static List<Type> GetReferenceChecklist(object DbContext)
+        {
+            List<Type> RefPropTypes = new List<Type>();
+
+            PropertyInfo[] infos = DbContext.GetType().GetProperties();
+            foreach (PropertyInfo info in infos)
+            {
+                //Console.WriteLine(info.PropertyType.Name.ToString());
+                if (info.PropertyType.Name.ToString().Contains("DbSet"))
+                {
+                    Console.WriteLine(info.PropertyType.GetGenericArguments()[0].Name);
+                    RefPropTypes.Add(info.PropertyType.GetGenericArguments()[0]);
+                }
+            }
+
+            return RefPropTypes;
+        }
+
+        public static bool Validator(object o)
+        {
+            PropertyInfo[] infos = o.GetType().GetProperties();
+            foreach (PropertyInfo info in infos)
+            {
+                var attrs = (DKDBCustomAttributes.DKDBMaxLengthAttribute[])info.GetCustomAttributes(
+                    typeof(DKDBCustomAttributes.DKDBMaxLengthAttribute), false);
+                foreach (var attr in attrs)
+                {
+                    Type t = info.PropertyType;
+                    if (attr.Length < ((String)(info.GetValue(o))).Length)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// max Length attribute for String properties to be validated by DKDB
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Property)]
+        public class DKDBMaxLengthAttribute : Attribute
+        {
+            public int Length { get; set; }
+        }
+    }
+}
---
+++ DKDB/DbContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DKDB;
+
+namespace DKDB
+{
+    public class DbContext
+    {
+        
+
+        public List<Type> dbsetTypes = new List<Type>();
+        //public List<TransactionRecord> transactionRecords = new List<TransactionRecord>();
+
+        /// <summary>
+        /// Fills the DbSet type list "dbsetTypes" for easier use if needed
+        /// </summary>
+        public void initSetTypes()
+        {
+            PropertyInfo[] infos = this.GetType().GetProperties();
+            foreach (PropertyInfo info in infos)
+            {
+                //String propTypeNameString = info.PropertyType.Name.ToString();
+                if (info.PropertyType.Name.ToString().Contains("DbSet"))
+                {
+                    Console.WriteLine(info.PropertyType.GetGenericArguments()[0].Name);
+                    dbsetTypes.Add(info.PropertyType.GetGenericArguments()[0]);
+                }
+            }
+        }
+
+        //Returns a specific DbSet for easier access
+        public object GetDBSetByType(Type t)
+        {
+            PropertyInfo[] infos = this.GetType().GetProperties();
+            foreach(PropertyInfo info in infos)
+            {
+                if(info.PropertyType.ToString().Contains("DbSet"))
+                {
+                    Type t3 = info.PropertyType.GetGenericArguments()[0];
+                    if (t == t3)
+                    {
+                        return info.GetValue(this);
+                    }
+                }
+            }
+            return null;
+        }
+
+        
+
+        public int SaveChanges()
+        {
+            PropertyInfo[] infos = this.GetType().GetProperties();
+            foreach(PropertyInfo info in infos)
+            {
+                String type_s = info.PropertyType.ToString();
+                if (type_s.Contains("DbSet"))
+                {
+                    Type d1 = typeof(DbSet<>);
+                    Type[] typeArgs = { info.PropertyType };
+                    Type constructed = d1.MakeGenericType(typeArgs);
+
+                    info.GetValue(this).GetType().GetMethod("SaveChanges");
+                }
+            }
+            return 0;
+        }
+    }
+
+    
+
+    //public class TransactionRecord
+    //{
+    //    public Type t;
+    //    public object originalState;
+    //    public PropertyInfo changedProperty;
+    //    public object value;
+    //    public String TransactionType;
+    //    public bool complex;
+    //    public int complexityOrder;
+
+    //    public TransactionRecord(object originalState, PropertyInfo changedProperty, object value, String TransactionType)
+    //    {
+    //        originalState = CloneObject(originalState);
+    //        t = originalState.GetType();
+    //        this.changedProperty = changedProperty;
+    //        this.value = value;
+    //        this.TransactionType = TransactionType;
+    //    }
+
+    //    public void OrderRecords(List<TransactionRecord> records)
+    //    {
+
+    //    }
+
+    //    public object CloneObject(object original)
+    //    {
+    //        object clone = Activator.CreateInstance(original.GetType());
+    //        foreach (PropertyInfo info in original.GetType().GetProperties())
+    //        {
+    //            info.SetValue(clone, info.GetValue(original));
+    //        }
+    //        return clone;
+    //    }
+    //}
+}
