35ed58c658ebbfa9d7605ce1433706d2d58088d3
keremozyer <keremozyer95@gmail.com>
2017-03-16 14:33:36 +0300

    Created Unit Test Project.

--- DKDB/DbSet.cs
+++ DKDB/DbSet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace DKDB
 {
@@ -93,7 +94,7 @@ namespace DKDB
 
         public int SaveChanges()
         {
-
+            return 0;
         }
     }
 }
